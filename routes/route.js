89a741b43899a41e85ec6a1fd70e9d1ec7814ab4
dadiprasanth <dadiprasanth24@gmail.com>
2022-11-07 21:30:53 +0530

    added function

--- routes/route.js
+++ routes/route.js
@@ -13,6 +13,22 @@ route.get("/",async(req,res)=>{
     res.render("form.ejs",{orderdata,customersdata,productsdata})
 }) 
 route.post("/placeorder",async(req,res)=>{
-    console.log(req.body,"jkncjkxznjcn")
-    res.status(200).send("data from post")
+    const customersdata=await customers.findOne({email:req.body.email})
+    const productsdata=await products.findOne({Product_id:req.body.Product_id})
+    console.log(req.body)
+    console.log(customersdata,productsdata)
+    if(productsdata.Available_quantity>=req.body.quantity){
+        const total=productsdata.Product_price
+        const avl=customersdata.balance
+        if(avl>=total){
+            await products.updateOne({Product_id:req.body.Product_id},{Available_quantity:productsdata.Available_quantity-req.body.quantity})
+            await customers.updateOne({email:req.body.email},{balance:avl-total})
+            res.redirect("/")
+
+
+        }
+
+    }
+    //console.log(req.body)
+    //res.status(200).send(req.body)
 })  
